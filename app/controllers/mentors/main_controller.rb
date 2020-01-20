87ef7cc295cdb9babee53ebedaed1469ff5469cf
gangelion <gangelion8@gmail.com>
2020-01-20 20:21:30 +0900

    mentorsのlanguageカラムに言語を追加

--- app/controllers/mentors/main_controller.rb
+++ app/controllers/mentors/main_controller.rb
@@ -3,20 +3,7 @@ class Mentors::MainController < ApplicationController
 
   end
 
-  def create
-    @mentor = Mentor.new
-    @mentors = Mentor.create(language_params)
-    if @mentors.save
-      redirect_to root_path, :notice => "Foo saved"
-    else
-      root_path
-    end
-  end
-
-
-  private
-
-  def language_params
-    params.permit(:language)
-  end
 end
+
+
+  
