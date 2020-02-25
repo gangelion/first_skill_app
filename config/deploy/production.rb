8654d9b8c7fc682f0049a5eca24cd12638027c90
gangelion <gangelion8@gmail.com>
2020-02-25 10:31:39 +0900

    deploy変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,8 @@
+server "18.178.33.110", user: "ec2-user", roles: %w{app db web}
+
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
@@ -6,10 +11,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server "18.178.33.110", user: "ec2-user", roles: %w{app db web}
 
-set :rails_env, "production"
-set :unicorn_rack_env, "production"
 
 # role-based syntax
 # ==================
