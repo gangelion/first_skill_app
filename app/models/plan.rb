02441d3cb77d12f90f81b2afde2d9397663323f8
gangelion <gangelion8@gmail.com>
2020-02-28 16:26:07 +0900

    planモデルのバリデーションを設定

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -4,4 +4,9 @@ class Plan < ApplicationRecord
   belongs_to :user
   has_many :plan_skill_tags, dependent: :delete_all
   has_many :skills, through: :plan_skill_tags
+
+  validates :title, presence: true, length: { in: 10..80 }
+  validates :description, presence: true, length: { maximum: 2000 }
+  validates :price, presence: true
+
 end
--- db/migrate/20200226062127_create_plans.rb
+++ db/migrate/20200226062127_create_plans.rb
@@ -3,7 +3,7 @@ class CreatePlans < ActiveRecord::Migration[5.2]
     create_table :plans do |t|
       t.string :title, null: false
       t.text :description, null: false
-      t.string :plan_image, null: false
+      t.string :plan_image
       t.integer :price, null: false
       t.references :user, null: false, foreign_key: true
       t.timestamps
