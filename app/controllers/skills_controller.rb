a4039cfe3288796f291b5ab3a2d54bc10e135583
gangelion <gangelion8@gmail.com>
2020-03-05 14:10:30 +0900

    スキルボタン画面にページネーションを設定

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,6 +1,6 @@
 class SkillsController < ApplicationController
   def index
     @skill = Skill.find(params[:id])
-    @skills = PlanSkillTag.where(skill_id: params[:id])
+    @skills = PlanSkillTag.where(skill_id: params[:id]).page(params[:page]).per(5)
   end
 end
