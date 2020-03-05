c717999dc56608148e57ee7ce60f61a0ed7da1c4
gangelion <gangelion8@gmail.com>
2020-03-05 11:15:54 +0900

    メイン画面のメンターを新着順に修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user_params, only: [:show, :edit, :update, :destroy]
   def index
-    @plans = Plan.group(:user_id).first(6)
+    @plans = Plan.group(:user_id).reverse
   end
 
   def show
