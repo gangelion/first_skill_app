d7f4152cff1798e1697472c6d3d8e46b7eb79457
gangelion <gangelion8@gmail.com>
2020-03-07 10:52:53 +0900

    tag入力欄をテキストボックスに変更

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -8,9 +8,17 @@ class Plan < ApplicationRecord
   has_many :skills, through: :plan_skill_tags
 
   accepts_nested_attributes_for :users_plans, allow_destroy: true
+  accepts_nested_attributes_for :skills, allow_destroy: true
 
   validates :title, presence: true, length: { in: 10..80 }
   validates :description, presence: true, length: { maximum: 2000 }
   validates :price, presence: true
+
+  before_save :find_or_create_skill
+
+  private
+    def find_or_create_skill
+      self.skills = self.skills.map {|skill| Skill.find_or_create_by(skill_set: skill.skill_set.strip)}
+    end
 
 end
