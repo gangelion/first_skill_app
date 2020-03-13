62fd6941cee575c7befadfe1ec2a9fd6df9f4e64
gangelion <gangelion8@gmail.com>
2020-03-13 16:57:27 +0900

    プラン編集、削除機能の統合テスト

--- spec/features/plan_spec.rb
+++ spec/features/plan_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 feature 'plan', type: :feature do
 	given(:user) { create(:user) }
-  scenario 'ログインからプラン投稿まで' do
+	given(:plan) { create(:plan) }
+  scenario 'ログインからプラン投稿、編集、削除まで' do
 		visit root_path
 		expect(page).to have_content('First Skill App')
 		expect(page).to have_content('ログイン')
@@ -24,12 +25,47 @@ feature 'plan', type: :feature do
 			click_link('メンタープランの登録はこちら')
 			expect(current_path).to eq new_plan_path
 			fill_in 'plan_title', with: 'テストタイトルです。'
+			fill_in 'formTagInput', with: 'HTML'
 			fill_in 'plan_description', with: 'テストです。'
       fill_in 'plan_price', with: 1000
       click_on '登録する'
     }.to change(Plan, :count).by(1)
     expect(current_path).to eq users_path
-    expect(page).to have_selector('.user__name_link')
+		expect(page).to have_selector('.user__name_link')
+		# プラン一覧確認
+		click_link('投稿したプラン')
+		expect(current_path).to eq plans_path
+		find(".plan__show_link_title").click
+		expect(current_path).to eq plan_path("#{plan.id - 1}")
+		# プラン編集
+		click_link('プランを編集する')
+		expect(current_path).to eq edit_plan_path("#{plan.id - 1}")
+		fill_in 'plan_title', with: 'プランタイトルを編集しました'
+		fill_in 'formTagInput', with: 'Ruby'
+		fill_in 'plan_description', with: 'プラン説明を編集しました'
+		fill_in 'plan_price', with: 10000
+		click_on('保存する')
+		expect(page).to have_content('プランタイトルを編集しました')
+		click_on('プランタイトルを編集しました')
+		expect(page).to have_content('プランタイトルを編集しました')
+		expect(page).to have_link('Ruby')
+		expect(page).to have_content('プラン説明を編集しました')
+		expect(page).to have_content(10000)
+		# プラン削除
+		click_link('プランを編集する')
+		click_on('削除する')
+		expect(page).to have_content('プランを削除しました')
   end
-
+	feature 'プラン一覧の確認' do
+		scenario 'プランがない時はプランが表示されない' do
+			visit new_user_session_path
+			fill_in 'user_email', with: user.email
+			fill_in 'user_password', with: user.password
+			find(".login__main_btn").click
+			click_link('投稿したプラン')
+			expect(current_path).to eq plans_path
+			expect(page).to have_no_link(".plan__show_link")
+			expect(page).to have_no_selector(".plan__show_link")
+		end
+	end
 end
